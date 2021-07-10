52d594c9e2c6b1e56b13808049d771b1e1cdba26
dalton77-get <85321730+dalton77-get@users.noreply.github.com>
2021-07-10 14:19:14 -0500

    Update __init__.py

--- add2/__init__.py
+++ add2/__init__.py
@@ -15,9 +15,9 @@ def compiles():
 @check50.check(compiles)
 def adds24():
     """Adds 2 and 4 correctly"""
-    check50.run.stdin("2").stdin("4").stdout("6\n").exit()
+    check50.run("./add2").stdin("2").stdin("4").stdout("6\n").exit()
 
 @check50.check(compiles)
 def adds36():
     """Adds 3 and 6 correctly (prints 9)"""
-    check50.run.stdin("3").stdin("6").stdout("9\n").exit()
+    check50.run("./add2").stdin("3").stdin("6").stdout("9\n").exit()
