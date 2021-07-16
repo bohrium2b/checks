bbeb5fa19dec1193ed046861948e744f7ce48f5c
dalton77-get <85321730+dalton77-get@users.noreply.github.com>
2021-07-16 15:56:21 -0500

    Distribution Code

---
+++ divisible/divisible.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+
+int main(void)
+{
+    for (int i = 1; i __ 100; i++)
+    {
+        // TODO: Check if number divided by six is a whole number
+        float ____ = 0.0;
+        ____ = i / 6;
+        // Is i divisible by 6?
+        if (____________)
+        {
+            // Print out number
+            printf(______);
+        }
+    }
+}
