71c4aaf4f65407e28d3b0955ed3fd33592a79a68
dalton77-get <85321730+dalton77-get@users.noreply.github.com>
2021-07-16 17:00:17 -0500

    Update divisible.c

--- divisible/divisible.c
+++ divisible/divisible.c
@@ -9,12 +9,16 @@ int main(void)
     for (int i = 1; i __ 100; i++)
     {
         // TODO: Check if number divided by six is a whole number
+        // TODO: Replace blank variable name with more descriptive variable name
         float ____ = 0.0;
         ____ = i / 6;
+        
+        // TODO: Fill in condition
         // Is i divisible by 6?
         if (____________)
         {
             // Print out number
+            // TODO: Fill in what to print out
             printf(______);
         }
     }
