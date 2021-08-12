f14e7516a717e6ab8a00efd882b9d6b719930541
dalton77-get <85321730+dalton77-get@users.noreply.github.com>
2021-08-12 15:33:10 -0500

    Create testing.c

---
+++ odd/testing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    int number = atoi(argv[1]);
+    bool out = isodd(number);
+    if (out == TRUE)
+    {
+        printf("Odd\n");
+    }
+    else
+    {
+        printf("Even\n");
+    }
+}
