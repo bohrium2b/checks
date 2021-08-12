a65a8111bc056882dc964e0681765fef7f7407fb
dalton77-get <85321730+dalton77-get@users.noreply.github.com>
2021-08-12 15:29:48 -0500

    Create __init__.py

---
+++ odd/__init__.py
@@ -0,0 +1,37 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    """odd.c exists"""
+    check50.exists("runoff.c")
+    check50.include("testing.c")
+
+
+@check50.check(exists)
+def compiles():
+    """odd compiles"""
+    check50.c.compile("odd.c", lcs50=True)
+    odd = re.sub("int\s+main", "int distro_main", open("odd.c").read())
+    testing = open("testing.c").read()
+    with open("odd_test.c", "w") as f:
+        f.write(odd)
+        f.write("\n")
+        f.write(testing)
+    check50.c.compile("odd_test.c", lcs50=True)
+
+@check50.check(compiles)
+def returns_odd_5():
+    """isodd returns odd when ran on 5"""
+    check50.run("./odd_test 5").stdout("[Oo]dd ?(\n)?", regex=True).exit()
+
+@check50.check(compiles)
+def returns_even_4():
+    """isodd returns even when ran on 8"""
+    check50.run("./odd_test 8").stdout("[Ee]ven( )?(\n)?", regex=True).exit()
+
+@check50.check(compiles)
+def returns_odd_9():
+    """isodd returns odd when ran on 9"""
+    check50.run("./odd_test 9").stdout("[Oo]dd( )?(\n)?", regex=True).exit()
